1aaa76f43a8e7d855604cf70e56aea6ea9a279ef
TrungTechh <deptraihah123@gmail.com>
2023-05-18 13:57:38 +0700

    fix error

--- app_without_css.py
+++ app_without_css.py
@@ -11,6 +11,8 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.metrics import mean_squared_error
 
+
+
 # global variable
 df = pd.read_csv('Life_Expectancy_Data.csv')
 years = pd.unique(df.Year)
@@ -334,6 +336,26 @@ def predict_lifeExpectancy():
 # What factors influence life expectancy the most
 def most_influence():
     st.markdown('## What factors influence life expectancy the most?')
+    feature_index = ["HIV/AIDS", "Adult Mortality", "Income composition of resources", "Schooling", "thinness 5-9 years"]
+    feature_value = [0.379045, 0.280811, 0.209,0.030993, 0.016]
+
+
+    fig = plt.figure(figsize=(10, 4))
+    sns.barplot(x=feature_value, y=feature_index)
+    plt.xlabel('Feature Importance Score')
+    plt.title("Visualizing Important Features")
+    st.pyplot(fig)
+
+
+    st.markdown("If a country want to improve their life expectency, the most important thing they should are:")
+
+    with st.container():
+        st.markdown("- Prevent HIV/AIDS ")
+        st.markdown("- Care in take care of adults")
+        st.markdown("- Optimal utilization of available resources")
+        st.markdown("- Invest in education")
+        st.markdown("- Focus on nutrition for children 5 - 9 years")
+
 
 # Does the higher the GDP, the longer the life expectancy of the country will increase?
 def higherGDP_longerLE():
@@ -362,6 +384,9 @@ def solution():
 
 
 #--------------------------run--------------------------
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 read_data()
 descriptive_statistic()
 data_exploration()
